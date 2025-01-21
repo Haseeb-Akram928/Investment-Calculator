4f85ec3fb240a892167a3191fed7a1501ed92823
Haseeb Akram <haseeebjutt@gmail.com>
2025-01-21 13:51:31 +0500

    added the Input.jsx file

---
+++ src/Input.jsx
@@ -0,0 +1,46 @@
+import { useState } from "react";
+
+export default function Input({ onChange, userInput }) {
+  return (
+    <section id="user-input">
+      <div className="input-group">
+        <p>
+          <label htmlFor="">Initial Investment</label>
+          <input
+            type="number"
+            required
+            value={userInput.initialInvestment}
+            onChange={(e) => onChange("initialInvestment", e.target.value)}
+          />
+        </p>
+        <p>
+          <label htmlFor="">Annual Investment</label>
+          <input
+            type="number"
+            required
+            value={userInput.annualInvestment}
+            onChange={(e) => onChange("annualInvestment", e.target.value)}
+          />
+        </p>
+        <p>
+          <label htmlFor="">Expected Return</label>
+          <input
+            type="number"
+            required
+            value={userInput.expectedReturn}
+            onChange={(e) => onChange("expectedReturn", e.target.value)}
+          />
+        </p>
+        <p>
+          <label htmlFor="">Duration</label>
+          <input
+            type="number"
+            required
+            value={userInput.duration}
+            onChange={(e) => onChange("duration", e.target.value)}
+          />
+        </p>
+      </div>
+    </section>
+  );
+}
